6ac514b607ab7ffe47e384546f6c9da5e83e95b0
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-01-06 17:10:49 +0100

    configure tab component to display the proper icon

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,6 +1,35 @@
-const Tab = () => {
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+const Tab = ({ tab, isFIlterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isFIlterTab && isActiveTab
+      ? {
+          backgroundColor: snap.color,
+          opacity: 0.5,
+        }
+      : {
+          backgroundColor: 'transparent',
+          opacity: 1,
+        };
   return (
-    <div>Tab</div>
-  )
-}
-export default Tab
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFIlterTab ? 'rounded-full glassmorhism' : 'rounded-4'
+      }`}
+      onClick={handleClick}
+      style={activeStyles}
+    >
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${isFIlterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'} `}
+      />
+    </div>
+  );
+};
+export default Tab;
