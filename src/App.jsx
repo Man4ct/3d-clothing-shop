49dfab6f0aa4d3cb05766cf657191df6fa1ecffd
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-01-07 11:02:40 +0100

    added vercel analytic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Canvas from './canvas'
 import Customizer from './pages/Customizer'
 import Home from './pages/Home'
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
   return (
     <main className="app transition-all ease-in">
       <Home/>
       <Canvas/>
       <Customizer/>
+      <Analytics/>
       </main>
   )
 }
